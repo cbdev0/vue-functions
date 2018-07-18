dd15c533254a61ebc87dae53178bb6171d1aed81
Chris Brown <chris.brown@requitest.com>
2018-07-18 11:16:39 -0400

    adding name input

---
+++ src/components/AzureLearning.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>{name}</p>
+    <input type="text" @input="handleNameInput">
+  </div>
+</template>
+<script>
+export default {
+  name: "AzureLearning",
+  data() {
+    return {
+      name: 'Chris'
+    }
+  }
+}
+
+function handleNameInput(input) {
+  this.name = input;
+}
+</script>
+<style>
+
+</style>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import AzureLearning from '@/components/AzureLearning'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/azure',
+      name: AzureLearning
+    },
     {
       path: '/',
       name: 'HelloWorld',
